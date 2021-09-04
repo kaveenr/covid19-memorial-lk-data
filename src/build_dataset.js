7f0659e50710c04e9cd5e86a956b8e79d8e9968b
Kaveen <u.k.k.rodrigo@gmaiDDl.com>
2021-09-04 18:56:22 +0530

    added dataset build

---
+++ src/build_dataset.js
@@ -0,0 +1,43 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const fs = require('fs');
+const moment = require('moment');
+const { saveDataSource } = require('./commons');
+
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+async function main() {
+
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    });
+
+    await doc.loadInfo();
+    console.log(`Sheet ${doc.title} Loaded`);
+
+    const masterRecords = doc.sheetsByTitle["Master Records"];
+    const masterRows = await masterRecords.getRows();
+    console.log(`Loaded ${masterRows.length} rows`);
+
+    const dataSet = [];
+    masterRows.filter((row) => row.Province).forEach((row) => {
+        dataSet.push({
+            indexKey: row.IndexKey, 
+            deathDate: moment(row.DeathDate,"YYYY/MM/DD").utcOffset(330).toISOString(true),
+            province: row.Province,
+            district: row.District,
+            city: row.City,
+            ageType: row.AgeType,
+            ageValue: row.AgeValue,
+            gender: row.Gender,
+            deathPlace: row.DeathPlace,
+            incarcerated: row.incarcerated,
+            sourceType: row.SourceType,
+            sourceRef: row.SourceRef
+        });
+    });
+    console.log(`Complied ${dataSet.length} items for saving`);
+    
+    saveDataSource("covid19_deaths", dataSet);
+}
+
+main();
