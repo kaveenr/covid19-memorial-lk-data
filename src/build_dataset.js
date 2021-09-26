4c459aa28b264fbe2ee281e77d417a6f3ef26933
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2021-09-26 08:58:30 +0530

    geo build.

--- src/build_dataset.js
+++ src/build_dataset.js
@@ -5,7 +5,32 @@ const fs = require('fs');
 const moment = require('moment');
 const { saveDataSource } = require('./commons');
 
-const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+function mapGeo(obj, type) {
+    if (type == "Provinces") {
+        return {
+            "id": obj.id,
+            "name_en": obj.name_en,
+            "name_si": obj.name_si,
+            "name_ta": obj.name_ta
+        }
+    } else if (type == "Districts") {
+        return {
+            "id": obj.id,
+            "province_id": obj.province_id,
+            "name_en": obj.name_en,
+            "name_si": obj.name_si,
+            "name_ta": obj.name_ta
+        }
+    } else {
+        return {
+            "id": obj.id,
+            "district_id": obj.district_id,
+            "name_en": obj.name_en,
+            "name_si": obj.name_si,
+            "name_ta": obj.name_ta
+        }
+    }
+}
 
 function mapItem(row) {
     try {
@@ -28,7 +53,9 @@ function mapItem(row) {
     }
 }
 
-async function main() {
+async function scrapeData() {
+
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
     await doc.useServiceAccountAuth({
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
         private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
@@ -48,6 +75,39 @@ async function main() {
     console.log(`Complied ${dataSet.length} items for saving`);
     
     saveDataSource("covid19_deaths", dataSet);
+}
+
+async function scrapeGeo() {
+    
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_GEO_SHEET_ID);
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    });
+    const secs = ["Provinces", "Districts", "Cities"];
+    
+    await doc.loadInfo();
+    console.log(`Sheet ${doc.title} Loaded`);
+
+    let result = {};
+    for (let sIdx = 0; sIdx < secs.length; sIdx++) {
+        const section = secs[sIdx];
+        const masterRecords = doc.sheetsByTitle[section];
+        const masterRows = await masterRecords.getRows();
+        console.log(`Loaded ${masterRows.length} rows for ${section}`);
+        let dataSet = []
+        masterRows.forEach((row) => {
+            dataSet.push(mapGeo(row, section));
+        });
+        result[section.toLowerCase()] = dataSet;
+    }
+    saveDataSource("geo_processed", result, false);
+}
+
+
+async function main() {
+    await scrapeData();
+    await scrapeGeo();
 }
 
 main();
--- src/commons.js
+++ src/commons.js
@@ -7,14 +7,16 @@ function getFileTimestamp(){
     return date.format("YYYY-MM-DD");
 }
 
-function saveDataSource(name, obj) {
+function saveDataSource(name, obj, saveTsv = true) {
     console.log(`Saving Data Source ${name} to file`);
     try{
         const timestamp = getFileTimestamp();
         const jsonString = JSON.stringify(obj, null, 4);
         fs.writeFileSync(`data/historical/${name}_${timestamp}.json`, jsonString);
         fs.writeFileSync(`data/${name}_latest.json`, jsonString);
-        fs.writeFileSync(`data/${name}_latest.tsv`, tsv.stringify(obj));
+        if (saveTsv) {
+            fs.writeFileSync(`data/${name}_latest.tsv`, tsv.stringify(obj));
+        }
     } catch (e) {
         throw new Error("Failed Saving Files " + e);
     }
